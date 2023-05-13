af8b074ec86e63412927c78d081cc97369956c60
tagirov-am <tagirov.am@list.ru>
2023-05-13 23:35:42 +0300

    HW4

--- service/api/exceptions.py
+++ service/api/exceptions.py
@@ -48,3 +48,14 @@ class UserNotFoundError(AppException):
         error_loc: tp.Optional[tp.Sequence[str]] = None,
     ):
         super().__init__(status_code, error_key, error_message, error_loc)
+
+
+class ItemNotFoundError(AppException):
+    def __init__(
+        self,
+        status_code: int = HTTPStatus.NOT_FOUND,
+        error_key: str = "user_not_found",
+        error_message: str = "User is unknown",
+        error_loc: tp.Optional[tp.Sequence[str]] = None,
+    ):
+        super().__init__(status_code, error_key, error_message, error_loc)
